6915c7d7c8087d9a1856e83eed9aab6b75e18abc
codys <bethelchioma11@gmail.com>
2026-01-16 22:53:31 +0100

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,16 @@
-import Navbar from './components/Navbar'
-import { Routes, Route, Navigate } from "react-router-dom"
-import Admin from './pages/Admin'
-import AddProduct from './components/Addproduct'
-import ListProduct from './components/ListProduct'
+import Navbar from './components/Navbar';
+import { Routes, Route, Navigate } from "react-router-dom";
+import Admin from './pages/Admin';
 
 const App = () => {
   return (
     <>
       <Navbar />
       <Routes>
-          <Route path="/" element={<Navigate to="/addproduct" />} />
-          <Route path='/' element={<Admin />}></Route>
-        <Route path='/addproduct' element={<AddProduct/>}/>
-        <Route path='/listproduct' element={<ListProduct/>}></Route>
+        <Route path="/" element={<Navigate to="/addproduct" />} />
+        <Route path="/*" element={<Admin />} />
       </Routes>
-
     </>
   )
 }
-
-export default App
+export default App;
--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -1,21 +1,20 @@
-import React from 'react'
-import Sidebar from '../components/Sidebar'
-import { Routes, Route } from "react-router-dom"
-import ListProduct from '../components/ListProduct'
-import AddProduct from '../components/Addproduct'
+import React from 'react';
+import Sidebar from '../components/Sidebar';
+import { Routes, Route } from 'react-router-dom';
+import ListProduct from '../components/ListProduct';
+import ProductAdd from '../components/ProductAdd';
 
 const Admin = () => {
   return (
     <div className="admin">
       <Sidebar />
-
       <Routes>
-        <Route path="addproduct" element={<AddProduct />} />
+        <Route path="addproduct" element={<ProductAdd />} />
         <Route path="listproduct" element={<ListProduct />} />
+        <Route path="" element={<ProductAdd />} />  {/* Default route inside Admin */}
       </Routes>
-
     </div>
-  )
-}
+  );
+};
 
-export default Admin
+export default Admin;
