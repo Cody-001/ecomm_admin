a994362cfd3096c9d55ecf14b341011606227133
codys <bethelchioma11@gmail.com>
2026-01-16 23:45:32 +0100

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,17 @@
 import Navbar from './components/Navbar';
 import { Routes, Route, Navigate } from "react-router-dom";
 import Admin from './pages/Admin';
-import AddProduct from './components/AddProduct';
 import ListProduct from './components/ListProduct';
+import ProductAdd from './components/ProductAdd';
 
 const App = () => {
   return (
     <>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Admin />} />
+        <Route path="/" element={<Navigate to="/addproduct" />} />
+        <Route path="/" element={<Admin />}/>
         <Route path="/*" element={<Admin />} />
-        <Route path='/addproduct' element={<AddProduct/>}/>
-        <Route path='/listproduct' element={<ListProduct/>}/>
       </Routes>
     </>
   )
